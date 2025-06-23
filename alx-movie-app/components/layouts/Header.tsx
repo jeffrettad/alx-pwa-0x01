dacad96ac1c6d8a7690fdb4b5b0c1d6ea9bdb26a
jeffrettad <Adedokunadesoye3040@gmail.com>
2025-06-23 03:17:04 -0700

    .

---
+++ alx-movie-app/components/layouts/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+    return(
+        <header className="bg-gray-900 text-white p-4">
+            <h1 className="text-2x1 font-bold">ALX Movie App</h1>
+        </header>
+    );
+};
+
+export default Header;
