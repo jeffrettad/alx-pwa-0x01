f9f690b124dab645fa5c571828e7e2264b835432
jeffrettad <Adedokunadesoye3040@gmail.com>
2025-06-23 03:32:42 -0700

    .

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,8 +1,11 @@
-const Button: React.FC = () => {
-    return (
-        <button className= "px-4 py-2 bg-blue-500 text-white rounded">
-            Click Me
-        </button>
-    );
-};
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
+  return (
+    <button onClick={action} className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300">
+      {title}
+    </button>
+  )
+}
+
 export default Button;
--- alx-movie-app/components/layouts/Layout.tsx
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import { ComponentProps } from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
 
-interface LayoutProps {
-    children: React.ReactNode;
-  }
-  
-  const Layout: React.FC<LayoutProps> = ({ children }) => {
-    return (
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">{children}</main>
-        <Footer />
-      </div>
-    );
-  };
-  
-  export default Layout;
+const Layout: React.FC<ComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout;
